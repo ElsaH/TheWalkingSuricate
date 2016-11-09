1ada81b5abaf785b8baaa39ae49eb97e6adc9e17
Support Technique <support@atoutsdigital.com>
2016-11-09 11:22:28 +0100

    Projet de base
    Ensemble des fichiers de base pour le projet eclipse avec l'importation
    de processing.
    Pensez à vérifier que la bibliothèque est bien importée.

---
+++ src/TheWalkingSuricate.java
@@ -0,0 +1,23 @@
+import processing.core.PApplet;
+
+public class TheWalkingSuricate extends PApplet {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("TheWalkingSuricate");
+	}
+	
+	public void settings() {
+		size(300,300);
+	}
+	
+	public void setup() {
+		fill(120,50,240);
+	}
+	
+	public void draw() {
+		ellipse(width/2,height/2,second(),second());
+	}
+
+}
